470ea01ef44725d9b8551665ae603425d8ca86a0
Socea Tiberiu <soceatiberiuv@gmail.com>
2019-02-28 12:31:21 +0200

    heroku deploy2

--- src/components/Signin/SignIn.js
+++ src/components/Signin/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://gentle-cliffs-48157.herokuapp.com/http://localhost:3000/signin', {
+        fetch('https://gentle-cliffs-48157.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
